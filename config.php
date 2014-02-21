34fe1573ef973933feeab4c08aaa80c02edeabd8
Fedor <mail@site-don.ru>
2014-02-21 16:55:52 +0400

    To make the core
    taken out of the folder «Application_core» - modules folder and
    config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-
+    if(!defined("APPLICATION_CORE")){ header("HTTP/1.0 404 Not Found"); die("access denied!");}
     define( 'SERVER_NAME', 'http://'.$_SERVER["SERVER_NAME"].'' );
     define( 'CURRENT_URL', 'http://'.$_SERVER["SERVER_NAME"].$_SERVER['REQUEST_URI'] );
     
     //folders
     define('ADMIN_DIR',     APPLICATION_CORE.'/admin/');
     define('CLASS_DIR',     APPLICATION_CORE.'/class/');
-    define('MODULES_DIR',   APPLICATION_CORE.'/modules/');
+    define('MODULES_DIR',   'modules/');
     define('PAGES_DIR',     APPLICATION_CORE.'/pages/');
     define('TEMP_DIR',      'templates/');
     define('DIALOG_DIR',    APPLICATION_CORE.'/dialog/');
@@ -21,11 +21,10 @@
     define('TEMPLATE','temp1');
 
     // mysql
-    define( 'DB_NAME',      'db' );
-    define( 'DB_USER',      'user' );
-    define( 'DB_PASSWORD',  'pass' );
+    define( 'DB_NAME',      'cathod' );
+    define( 'DB_USER',      'root' );
+    define( 'DB_PASSWORD',  'root' );
     define( 'DB_LOCATION',  'localhost' );
 
     define( 'LANGUAGE', 'ru' );
-
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?
-	error_reporting (E_ALL);
+<?php
 	ini_set('display_errors', 1);
+        error_reporting (E_ALL);
 	//error_reporting( E_ERROR );
 
 	define( 'APPLICATION_CORE', 'application_core_1.0' );
 	
-	include_once(APPLICATION_CORE . '/config.php');
+	include_once('config.php');
 	include_once(APPLICATION_CORE . '/header.php');
 ?>
