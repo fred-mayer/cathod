c66022b3efa73763a96b9b55cefad0c9b9aef2bd
mizko85 <mizko@192.168.1.53>
2014-03-06 16:42:53 +0400

    изменения в printAdminPanel

--- application_core_1.0/class/template.php
+++ application_core_1.0/class/template.php
@@ -488,25 +488,10 @@ class TTemplate
     </div>';
     }
     function printAdminPanel(){
-        if($this->auth->isAdmin){
-            ?>
-                <div id="admin-panel">
-                    <p>Админ панель</p>
-                    <div class="btn-toolbar-admin">
-                        <div class="btn-group">
-                            <a href="?preview" class="btn">Предварительный просмотр</a>
-                            <button class="btn" module="admin" action="newpage">Новая страница</button>
-                            <button class="btn" module="admin" action="editpage" idpage="<? echo $this->idpage; ?>">Редактировать страницу</button>
-                            <button class="btn" module="admin" action="copypage" idpage="<? echo $this->idpage; ?>">Клонировать страницу</button>
-							<button class="btn" module="admin" action="listmodule">Список модулей</button>
-							<button class="btn btn-primary" module="admin" action="exitadmin">Выйти</button>
-                        </div>
-                    </div>
-                </div>
-                <div id="admin-sub-panel">
-                    <span id="shText" class="btn btn-danger">Админ</span>
-                </div>
-            <?
+        if ( $this->auth->isAdmin )
+        {
+            $module = $this->getModule( 'admin' );
+            $module->display( $this );
         }
         ?><div class="modal admin-modal" id="myModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display:none;"></div><?
     }
